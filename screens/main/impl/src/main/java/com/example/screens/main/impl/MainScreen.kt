7f1de4986d6caf3333370b6fd5e7afb2bbf227b0
ZhestovskyYS <aprilgoat17@gmail.com>
2023-06-03 17:14:33 +0600

    Init 2-lesson branch

---
+++ screens/main/impl/src/main/java/com/example/screens/main/impl/MainScreen.kt
@@ -0,0 +1,13 @@
+package com.example.screens.main.impl
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import java.lang.reflect.Modifier
+
+@Composable
+@Preview(showSystemUi = true)
+fun MainScreen(
+    modifier: Modifier = Modifier()
+) {
+
+}
